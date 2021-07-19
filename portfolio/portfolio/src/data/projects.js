d0e5a5271cda13b91e6bcd5373797e40ac8fc635
Liam O'Casey <locasey@outlook.com>
2021-07-19 13:09:34 -0400

    Project

---
+++ portfolio/portfolio/src/data/projects.js
@@ -0,0 +1,32 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Tic Tac Toe',
+        description: 'PLay Tic Tac Toe Against a Robot!',
+        link: 'https://github.com/locasey/TicTacToe',
+        image: project1
+    },
+
+    {
+        id: 2,
+        title: 'Example React App',
+        description: 'A React App',
+        link: 'https://google.com',
+        image: project2
+    },
+
+    {
+        id: 3,
+        title: 'Example React App',
+        description: 'A React App',
+        link: 'https://cnn.com',
+        image: project3
+    }
+
+];
+
+export default PROJECTS;
