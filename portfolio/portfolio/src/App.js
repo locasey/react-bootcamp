016b4d34b90ce9890c533a84d01f7db5123c4953
Liam O'Casey <locasey@outlook.com>
2021-07-19 12:34:01 -0400

    Made the Component Myself

--- portfolio/portfolio/src/App.js
+++ portfolio/portfolio/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super(); //Inheritance
+        this.state = { displayBio: false };
+        //console.log('Component this', this);
+
+        this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+
+    }
+    toggleDisplayBio() {
+        this.setState({ displayBio: !this.state.displayBio }); //use set state
+    }
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Liam</p>
+                <p>Working on front end dev</p>
+                {this.state.displayBio ? (
+                    <div>
+                        <p>I live in NOVA, </p>
+                        <p>My favorite language is python</p>
+                        <p>I also love running and cooking</p>
+                        <button onClick={this.toggleDisplayBio}>Say less</button>
+                    </div>
+                ) : (
+                    <div>
+                        <button onClick={this.toggleDisplayBio}> Read more </button>
+                    </div>
+                )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
